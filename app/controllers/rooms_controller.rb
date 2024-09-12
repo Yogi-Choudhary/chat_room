eae01bab8eee68617fe50aecd1a04e30e8006c87
yogesh <yogichoudhary9715@gmail.com>
2024-09-12 10:40:57 +0530

    show a room in index file

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,11 +4,12 @@ class RoomsController < ApplicationController
   def index
     @room = Room.new
     @rooms = Room.public_room
+    @single_room = Room.find(params[:id])
     @users = User.all_except(current_user)
   end
 
   def create
     @room = Room.create(name: params["room"]["name"])
   end
-  
+
 end
