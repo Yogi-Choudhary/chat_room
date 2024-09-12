7c52424e36095a63253836184661dc5e203e4015
yogesh <yogichoudhary9715@gmail.com>
2024-09-12 14:45:43 +0530

    some code remove in devise.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -37,7 +37,7 @@ Devise.setup do |config|
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
-  config.parent_controller = 'TurboDeviseController'
+  # config.parent_controller = 'TurboDeviseController'
   # config.parent_controller = 'DeviseController'
 
 
