ec7d5b22d3b77c787e05f765b50f0136417e0408
yogesh <yogichoudhary9715@gmail.com>
2024-09-12 12:25:34 +0530

    add showw method in room controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,9 +3,20 @@ class RoomsController < ApplicationController
 
   def index
     @room = Room.new
-    @rooms = Room.public_room
-    @single_room = Room.find(params[:id])
+    @rooms = Room.public_rooms
     @users = User.all_except(current_user)
+    render "index"
+  end
+
+  def show
+    @single_room = Room.find(params[:id])
+    @room = Room.new
+    @rooms = Room.public_rooms
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+    @users = User.all_except(current_user)
+
+    render "index"
   end
 
   def create
